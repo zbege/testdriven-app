1291405944566b63a031f248eb69b2f41ec4820a
Zoltan Bege <begezoltan@gmail.com>
2018-11-16 21:41:33 +0200

    First part

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,17 +1,35 @@
 # services/users/project/__init__.py
 
-from flask import Flask, jsonify
 
-# instantiate the app
-app = Flask(__name__)
+import os
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# instantiate the db
+db = SQLAlchemy()
+
+
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
