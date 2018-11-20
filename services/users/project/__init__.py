e19351ab4fa95dbacbbc81ad561f61a27298537b
Zoltan Bege <begezoltan@gmail.com>
2018-11-20 21:35:36 +0200

    Flask Migrate

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,14 +4,16 @@
 import os
 
 from flask import Flask
-from flask_cors import CORS
-from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
+from flask_migrate import Migrate  # new
 
 
 # instantiate the extensions
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -20,7 +22,7 @@ def create_app(script_info=None):
     app = Flask(__name__)
 
     # enable CORS
-    CORS(app)  # new
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
@@ -29,6 +31,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    migrate.init_app(app, db)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
